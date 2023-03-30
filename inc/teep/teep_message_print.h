6ece4fc463dadd7d05bac85bd0984e188e80c621
Ken Takayama <ken.takayama.ietf@gmail.com>
2023-03-30 00:51:10 +0000

    add: a way to print the meaning inth context of a value

--- inc/teep/teep_message_print.h
+++ inc/teep/teep_message_print.h
@@ -16,9 +16,9 @@
 #define TEEP_MAX_PRINT_BYTE_COUNT       64
 #define TEEP_MAX_PRINT_TEXT_COUNT       16
 
-const char *teep_err_to_str(teep_err_t err);
-const char *teep_err_code_to_str(int32_t err_code);
-const char *teep_cose_algs_key_to_str(int64_t cose_algs_key);
+char *teep_err_to_str(teep_err_t err);
+char *teep_err_code_to_str(int32_t err_code);
+char *teep_cose_algs_key_to_str(int64_t cose_algs_key);
 teep_err_t teep_print_query_request(const teep_query_request_t *query_request, uint32_t indent_space, uint32_t indent_delta);
 teep_err_t teep_print_query_response(const teep_query_response_t *query_response, uint32_t indent_space, uint32_t indent_delta);
 teep_err_t teep_print_update(const teep_update_t *teep_update, uint32_t indent_space, uint32_t indent_delta, const unsigned char *ta_public_key);
@@ -26,28 +26,31 @@ teep_err_t teep_print_success(const teep_success_t *success, uint32_t indent_spa
 teep_err_t teep_print_error(const teep_error_t *error, uint32_t indent_space, uint32_t indent_delta);
 teep_err_t teep_print_message(const teep_message_t *msg, uint32_t indent_space, uint32_t indent_delta, const unsigned char *ta_public_key);
 
-const char* teep_eat_claim_label_to_str(const int64_t label, const int64_t n);
-const char* teep_position_to_str(const int64_t label, const int64_t type);
+typedef char* (*TeepLabelToStr)(const int64_t, const int64_t);
+typedef char* (*TeepPrintValue)(void *val);
+
+char* teep_eat_claim_label_to_str(const int64_t label, const int64_t n);
+char* teep_position_label_to_str(const int64_t label, const int64_t type);
 
 void teep_print_map(QCBORDecodeContext *context,
                QCBORItem *item,
                const uint32_t indent_space,
                const uint32_t indent_delta,
-               const char* (*label_to_str)(int64_t, int64_t),
+               TeepLabelToStr label_to_str,
                const int64_t label,
                bool in_header);
 void teep_print_array(QCBORDecodeContext *context,
                  QCBORItem *item,
                  const uint32_t indent_space,
                  const uint32_t indent_delta,
-                 const char* (*label_to_str)(int64_t, int64_t),
+                 TeepLabelToStr label_to_str,
                  const int64_t position,
                  bool in_hdeader);
 
 teep_err_t teep_print_cose_usefulbufc(UsefulBufC cose,
                                         const uint32_t indent_space,
                                         const uint32_t indent_delta,
-                                        const char* (*label_to_str)(int64_t, int64_t));
+                                        TeepLabelToStr label_to_str);
 teep_err_t teep_print_cose_eat(UsefulBufC cose_eat,
                            const uint32_t indent_space,
                            const uint32_t indent_delta);
